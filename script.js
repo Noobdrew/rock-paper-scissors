c86c7b2cd852655636fdba0f1985f4daf4be9a93
Noobdrew <noobdrew23@gmail.com>
2022-12-27 15:30:20 +0200

    compChoise function is complete
    Computer gets randomly assigned rock, paper or scissors

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
 // set computer choise function by returning a 
 //random number between 1-3 and convert to "rock" "paper" "scissors"
 
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max + 1);
+}
+
+
+function computerChoise() {
+    let compChoise = getRandomInt(3);
+    let choise = ""
+    
+    if (compChoise === 1){
+        choise = "Rock";
+    } else if (compChoise === 2){
+        choise = "Paper"
+    } else {
+        choise = "Scissors"
+    }
+
+    console.log(choise)
+}
+
+computerChoise()
 //let player choose  "rock" "paper" "scissors" by typing the name of their choise
 
 
