9d99f93fd26b79f04a4a345036de296a5df256eb
Noobdrew <noobdrew23@gmail.com>
2022-12-27 15:18:34 +0200

    algorithm complete in comments

---
+++ script.js
@@ -0,0 +1,9 @@
+// set computer choise function by returning a 
+//random number between 1-3 and convert to "rock" "paper" "scissors"
+
+//let player choose  "rock" "paper" "scissors" by typing the name of their choise
+
+
+// check if player choise beats computer choise
+
+// display a you win or you lost massage
